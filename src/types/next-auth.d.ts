ab33b413e10b9f7e8d4286ee835ba69822781ac5
oscar-chj <haojchung@gmail.com>
2025-12-06 03:00:29 +0800

    fix: added role to auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,6 +4,7 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      role?: string;
     } & DefaultSession["user"];
   }
 
@@ -12,11 +13,13 @@ declare module "next-auth" {
     email: string;
     name?: string | null;
     image?: string | null;
+    role?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
+    role?: string;
   }
 }
